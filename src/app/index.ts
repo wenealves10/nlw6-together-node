53b3ada5de7fe81d47b14a76caa37a3fcbb5cb4d
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-26 18:28:40 -0300

    Index main 👍

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { router } from "../routes/create.user.routes";
+import "express-async-errors";
+import myError from "../error/error";
+import { router } from "../routes/routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
-
+app.use(myError.error);
 export { app };
