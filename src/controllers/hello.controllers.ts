01201796ec3f406342bc9fe14bbf7ec7e72179f5
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-26 11:25:10 -0300

    Deleted 💯

--- src/controllers/hello.controllers.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export class HelloControllers {
-	async hello(request: Request, response: Response) {
-		response.json({ message: "Hello World API" });
-	}
-}
