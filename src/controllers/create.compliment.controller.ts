811838cb46c3a8f77d1de52bea9f8906ed9cd9e6
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-27 17:01:48 -0300

    Create Compliment Controller 💯

---
+++ src/controllers/create.compliment.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { CreateComplimentService } from "../services/create.compliment.service";
+
+class CreateComplimentController {
+	async handle(request: Request, response: Response) {
+		const { tag_id, user_sender, user_receiver, message } = request.body;
+
+		const createComplimentService = new CreateComplimentService();
+
+		const compliment = await createComplimentService.execute({
+			tag_id,
+			user_sender,
+			user_receiver,
+			message,
+		});
+
+		return response.json(compliment);
+	}
+}
+export { CreateComplimentController };
