cc075050c0295855c7e2f26e1bb068da4419b0f0
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-27 17:04:26 -0300

    Modify Create User Service 👍

--- src/services/create.user.service.ts
+++ src/services/create.user.service.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm";
+import { hash } from "bcryptjs";
 import { UsersRepositories } from "../repositories/users.repositories";
 
 interface IUserRequest {
 	name: string;
 	email: string;
+	password: string;
 	admin?: boolean;
 }
 
 class CreateUserService {
-	async execute({ name, email, admin }: IUserRequest) {
+	async execute({ name, email, admin = false, password }: IUserRequest) {
 		const usersRepository = getCustomRepository(UsersRepositories);
 
 		if (!email) {
@@ -23,10 +25,13 @@ class CreateUserService {
 			throw new Error("User Already Exists");
 		}
 
+		const passwordHash = await hash(password, 8);
+
 		const user = usersRepository.create({
 			name,
 			email,
 			admin,
+			password: passwordHash,
 		});
 
 		await usersRepository.save(user);
