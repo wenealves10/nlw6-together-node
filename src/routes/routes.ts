7c34680628e15e19bdf0f91e39d8c90abdd9b9b9
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-27 17:03:43 -0300

    Added more routes :100:

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "../controllers/authenticate.user.controller";
+import { CreateComplimentController } from "../controllers/create.compliment.controller";
 import { CreateTagsController } from "../controllers/create.tag.controller";
 import { CreateUsersController } from "../controllers/create.user.controller";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
@@ -7,8 +9,12 @@ const router = Router();
 
 const createUserController = new CreateUsersController();
 const createTagController = new CreateTagsController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", ensureAdmin, createComplimentController.handle);
 
 export { router };
